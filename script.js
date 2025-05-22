af3f7d8f4b7d5d81da9476f28ba5fda851387e0e
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 15:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+	const strong = document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
 
+	strong.forEach(el => {
+		el.style.color = 'rgb(0, 128, 0)';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	
+	strong.forEach(el => {
+		el.style.color = 'rgb(0, 0, 0)';
+	});
     
 }
